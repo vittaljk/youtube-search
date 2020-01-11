67d65db8c17e3fb40d0d09fb2a5c019458979f64
vittaljk <vittaljk@gmail.com>
2020-01-12 04:17:38 +0530

    embedding video now

--- src/components/Video/VideoDetail/VideoDetail.js
+++ src/components/Video/VideoDetail/VideoDetail.js
@@ -5,7 +5,13 @@ function VideoDetail({ video }) {
     const url = `https://www.youtube.com/embed/${video.id.videoId}`;
     return (
         <div>
-            {url}
+            <div className="embed-wrapper">
+                <iframe title="youtube-embedded-frane" src={url}></iframe>
+            </div>
+            <div className="info">
+                <div className="title">{video.snippet.title}</div>
+                <div className="description">{video.snippet.description}</div>  
+            </div>
         </div>
     )
 }
