4a2ade4bb98fc4bf5b55ff60dce34f7eb9affe97
vittaljk <vittaljk@gmail.com>
2020-01-12 23:09:17 +0530

    api testing done

--- src/api/__tests__/youtube.test.js
+++ src/api/__tests__/youtube.test.js
@@ -1,13 +1,36 @@
 const youtube = require('../youtube')
+const searchText = 'cat';
+
+function checkIfTitleIncludes(title) {
+    return title.includes('searchText');
+}
 
 describe('#getVideos() from youtube api', () => {
     it('should load videos', async () => {
-        const data = await youtube.getVideos('surffing')
-        console.log('=================== data ==============');
-        // console.log(data.entity);
-        // console.log(data.entity.items[0].snippet.title);
+        const data = await youtube.getVideos('searchText')
         expect(data).toBeDefined();
         expect(data.status.code).toEqual(200);
-        // expect(data.entity.items[0].snippet.title).toMatch(/surffing/i)
+    })
+})
+
+describe('#getVideos() from youtube api', () => {
+    it(`should load videos related to ${searchText}`, async () => {
+        const data = await youtube.getVideos('searchText')
+        expect(data).toBeDefined();
+        expect(data.status.code).toEqual(200);
+        const titles = data.entity.items.map(item => item.snippet.title);
+        let result = titles.some(checkIfTitleIncludes);
+        expect(result).toBe(true)
+    })
+})
+
+describe('#getVideos() from youtube api', () => {
+    it(`should load videos related to ${searchText} only`, async () => {
+        const data = await youtube.getVideos('searchText')
+        expect(data).toBeDefined();
+        expect(data.status.code).toEqual(200);
+        const titles = data.entity.items.map(item => item.snippet.title);
+        let result = titles.every(checkIfTitleIncludes);
+        expect(result).toBe(true)
     })
 })
