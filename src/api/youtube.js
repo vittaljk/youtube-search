5c5c865fc0a80a0d505dd54b1d7fe6d5e1b63532
vittaljk <vittaljk@gmail.com>
2020-01-12 23:57:44 +0530

    api for pagination

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,4 +2,4 @@ import request from './request'
 
 const getVideos = searchText => request(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&part=snippet&type=video&maxResults=50&q=${searchText}`)
 
-export { getVideos }
+export { getVideos } 
