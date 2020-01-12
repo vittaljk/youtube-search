f4015f1661c2d4fb94d5bb1d93e882d0f738ed32
vittaljk <vittaljk@gmail.com>
2020-01-13 00:41:36 +0530

    fixed test cases

--- src/api/__tests__/youtube.test.js
+++ src/api/__tests__/youtube.test.js
@@ -1,13 +1,13 @@
 const youtube = require('../youtube')
-const searchText = 'cat';
+const searchText = 'surffing';
 
 function checkIfTitleIncludes(title) {
-    return title.includes('searchText');
+    return title.includes(searchText);
 }
 
 describe('#getVideos() from youtube api', () => {
     it('should load videos', async () => {
-        const data = await youtube.getVideos('searchText')
+        const data = await youtube.getVideos(searchText)
         expect(data).toBeDefined();
         expect(data.status.code).toEqual(200);
     })
@@ -15,7 +15,7 @@ describe('#getVideos() from youtube api', () => {
 
 describe('#getVideos() from youtube api', () => {
     it(`should load videos related to ${searchText}`, async () => {
-        const data = await youtube.getVideos('searchText')
+        const data = await youtube.getVideos(searchText)
         expect(data).toBeDefined();
         expect(data.status.code).toEqual(200);
         const titles = data.entity.items.map(item => item.snippet.title);
@@ -26,7 +26,7 @@ describe('#getVideos() from youtube api', () => {
 
 describe('#getVideos() from youtube api', () => {
     it(`should load videos related to ${searchText} only`, async () => {
-        const data = await youtube.getVideos('searchText')
+        const data = await youtube.getVideos(searchText)
         expect(data).toBeDefined();
         expect(data.status.code).toEqual(200);
         const titles = data.entity.items.map(item => item.snippet.title);
