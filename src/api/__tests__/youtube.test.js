865140ca067a8ed188cc6816df8725b09ee79391
vittaljk <vittaljk@gmail.com>
2020-01-13 22:56:37 +0530

    updated test files

--- src/api/__tests__/youtube.test.js
+++ src/api/__tests__/youtube.test.js
@@ -11,26 +11,13 @@ describe('#getVideos() from youtube api', () => {
         expect(data).toBeDefined();
         expect(data.status.code).toEqual(200);
     })
-})
-
-describe('#getVideos() from youtube api', () => {
+    
     it(`should load videos related to ${searchText}`, async () => {
         const data = await youtube.getVideos(searchText)
         expect(data).toBeDefined();
         expect(data.status.code).toEqual(200);
         const titles = data.entity.items.map(item => item.snippet.title);
         let result = titles.some(checkIfTitleIncludes);
-        expect(result).toBe(true)
-    })
-})
-
-describe('#getVideos() from youtube api', () => {
-    it(`should load videos related to ${searchText} only`, async () => {
-        const data = await youtube.getVideos(searchText)
-        expect(data).toBeDefined();
-        expect(data.status.code).toEqual(200);
-        const titles = data.entity.items.map(item => item.snippet.title);
-        let result = titles.every(checkIfTitleIncludes);
         expect(result).toBe(true)
     })
 })
