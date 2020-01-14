fbf133e3e4578e8bce687cca6f7b6ef7f69557b7
vittaljk <vittaljk@gmail.com>
2020-01-14 16:37:50 +0530

    simulating search input

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App.js';
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
+const searchText = 'surffing';
+const youtube = require('./api/youtube');
 
 describe('<App /> loading', () => {
     it("renders without crashing", () => {
@@ -8,4 +13,14 @@ describe('<App /> loading', () => {
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+})
+
+describe('<SearchBar />', () => {
+    it('simulating search bar input', async () => {
+        const wrapper = mount(<App />);
+        wrapper.find('input').simulate('change', { target: { value: searchText } });
+        expect(wrapper.find("input").instance().value).toEqual(searchText);
+
+        // invoke search handler
+    })
 })
--- src/api/__tests__/youtube.test.js
+++ src/api/__tests__/youtube.test.js
@@ -6,21 +6,18 @@ function checkIfTitleIncludes(title) {
 }
 
 describe('#getVideos() from youtube api', () => {
-    // it('should load videos', async () => {
-    //     const data = await youtube.getVideos(searchText)
-    //     expect(data).toBeDefined();
-    //     expect(data.status.code).toEqual(200);
-    // })
+    it('should load videos', async () => {
+        const data = await youtube.getVideos(searchText)
+        expect(data).toBeDefined();
+        expect(data.status.code).toEqual(200);
+    })
     
-    // it(`should load videos related to ${searchText}`, async () => {
-    //     const data = await youtube.getVideos(searchText)
-    //     expect(data).toBeDefined();
-    //     expect(data.status.code).toEqual(200);
-    //     const titles = data.entity.items.map(item => item.snippet.title);
-    //     let result = titles.some(checkIfTitleIncludes);
-    //     expect(result).toBe(true)
-    // })
-    it('should pass', () => {
-        expect(true).toBe(true);
+    it(`should load videos related to ${searchText}`, async () => {
+        const data = await youtube.getVideos(searchText)
+        expect(data).toBeDefined();
+        expect(data.status.code).toEqual(200);
+        const titles = data.entity.items.map(item => item.snippet.title);
+        let result = titles.some(checkIfTitleIncludes);
+        expect(result).toBe(true)
     })
 })
